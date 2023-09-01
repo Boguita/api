6d7b17ceddf384492523a91ed50efa86506bda8b
Boguita <heberdgomez@hotmail.com>
2023-09-01 20:44:00 -0300

    add true credentials

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ app.use(
 
 app.use((req, res, next) => {
   
-  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Credentials", true);
   // Otras cabeceras CORS si es necesario
   res.setHeader(
     "Access-Control-Allow-Headers",
