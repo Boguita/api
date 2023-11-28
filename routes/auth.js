05dae25f68ea2636dcc52e8d191c6c2cc40280a0
Boguita <heberdgomez@hotmail.com>
2023-11-28 00:05:04 -0300

    Remove unused registerAdmin route

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,12 @@
 import express from "express";
-import { register, login, logout, loginAdmin, registerAdmin, passwordForgot, resetPassword} from "../controllers/auth.js";
+import { register, login, logout, loginAdmin,passwordForgot, resetPassword} from "../controllers/auth.js";
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/forgot-password", passwordForgot);
 router.post("/reset-password/:id/:token", resetPassword);
-router.post("/registeradmin", registerAdmin);
+// router.post("/registeradmin", registerAdmin);
 router.post("/login", login);
 router.post("/login-aubenefits", loginAdmin);
 router.post("/logout", logout);
